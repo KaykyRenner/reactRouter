6ae568f5b4ba5110a6384ca7a74cb67bc49892ea
Kayky Renner Siqueira Galindo <kayky.renner.sg@gmail.com>
2025-02-25 12:52:04 -0300

    react router

--- REACTROUTER/src/router/pages/detailsContact.jsx
+++ REACTROUTER/src/router/pages/detailsContact.jsx
@@ -1,9 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 const ContactDetails = () => {
   const { id } = useParams();
+  //6- redirect
+  const navigate = useNavigate()
+  const handleContact = ()=>{
+    console.log('contato enviado')
+    return navigate("/")
+  }
   return (
     <div>
       <h1>contactDetails :{id}</h1>
+  
+      <button onClick={handleContact}>enviar mensagem</button>
     </div>
   );
 };
